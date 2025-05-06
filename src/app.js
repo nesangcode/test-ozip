10b4bef4940c2c2dafbb8aa6a5c9a8529956ee62
RaihanPrasetia <raihanprasetia86@gmail.com>
2025-05-06 11:15:26 +0700

    update delete

--- src/app.js
+++ src/app.js
@@ -20,7 +20,6 @@ function addTask() {
   });
 
   saveTasks(tasks);
-
   taskInput.value = "";
   renderTasks();
 }
@@ -34,13 +33,25 @@ function renderTasks() {
 
   filteredTasks.forEach((task) => {
     const indexInAllTasks = tasks.indexOf(task);
+
     const li = document.createElement("li");
     li.textContent = task.text;
 
     if (task.completed) {
       li.style.textDecoration = "line-through";
     }
+
     li.addEventListener("click", () => toggleTask(indexInAllTasks));
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "🗑️";
+    deleteBtn.style.marginLeft = "10px";
+    deleteBtn.addEventListener("click", (e) => {
+      e.stopPropagation(); // agar tidak trigger toggleTask
+      deleteTask(indexInAllTasks);
+    });
+
+    li.appendChild(deleteBtn);
     taskList.appendChild(li);
   });
 }
@@ -71,4 +82,10 @@ function saveTasks(tasks) {
 function loadTasks() {
   const tasks = window.localStorage.getItem(STORAGE_KEY);
   return tasks ? JSON.parse(tasks) : [];
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  saveTasks(tasks);
+  renderTasks();
 }
